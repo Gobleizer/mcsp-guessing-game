d3b35d5b991769924a4dc5e7e2baf87d45d77894
Kevin Goble <goble.kevin@gmail.com>
2023-11-13 09:43:11 -0800

    Feature 0 of guessing game complete

--- app.js
+++ app.js
@@ -1,33 +1,17 @@
 // const userName = prompt("What is your name?");
 
 function play() {
-  let secretNumber = Math.floor(Math.random() * 10) + 1;
-  let prevGuesses = [];
+  let secretNumber = 5;
+
   while(true) {
     const guess = prompt("Guess a number.");
     if(guess === null) {
       alert("Goodbye!");
       break;
     }
-
-    const numGuess = Number(guess);
-    
-    if(Number.isNaN(numGuess)) {
-      alert("Please enter a valid number");
-    } else if(numGuess < secretNumber) {
-      prevGuesses.push(numGuess);
-      alert(`Sorry ${userName}, guess higher`);
-    } else if(numGuess > secretNumber) {
-      prevGuesses.push(numGuess);
-      alert(`Sorry ${userName}, guess lower`);
-    } else {
-      alert(`Correct! Your previous guess(es) were ${prevGuesses.join(", ")}.`);
-      const playAgain = prompt("Do you want to play again?");
-      if(playAgain.toLowerCase() === "yes") {
-        play();
-      } else {
-        break;
-      }
+    if (guess == secretNumber) {
+      alert("You are RIGHT!")
+      break;
     }
   }
 }
